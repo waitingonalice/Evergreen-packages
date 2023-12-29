e77e769b78c68862db8fb6ed8a36f931114b2b00
Wilson <fcsie55@gmail.com>
2023-12-30 00:18:19 +0800

    chore: integrate with shadcn and build new components

--- design-system/src/components/resizable/Resizable.stories.tsx
+++ design-system/src/components/resizable/Resizable.stories.tsx
@@ -56,20 +56,51 @@ export function ThreeGridHorizontal() {
     >
       <ResizablePanel defaultSize={25}>
         <div className="flex h-full items-center justify-center p-6">
-          <span className="font-semibold">Header</span>
+          <span className="font-semibold">One</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={50}>
+        <div className="flex h-full items-center  justify-center p-6">
+          <span className="font-semibold">Two</span>
         </div>
       </ResizablePanel>
       <ResizableHandle withHandle />
       <ResizablePanel defaultSize={75}>
         <div className="flex h-full items-center  justify-center p-6">
-          <span className="font-semibold">Content</span>
+          <span className="font-semibold">Three</span>
         </div>
       </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={75}>
-        <div className="flex h-full items-center  justify-center p-6">
-          <span className="font-semibold">Content</span>
+    </ResizablePanelGroup>
+  );
+}
+
+export function ThreeGridMixed() {
+  return (
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="max-w-md rounded-lg border"
+    >
+      <ResizablePanel defaultSize={50}>
+        <div className="flex h-[200px] items-center justify-center p-6">
+          <span className="font-semibold">One</span>
         </div>
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel defaultSize={50}>
+        <ResizablePanelGroup direction="vertical">
+          <ResizablePanel defaultSize={25}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Two</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel defaultSize={75}>
+            <div className="flex h-full items-center justify-center p-6">
+              <span className="font-semibold">Three</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </ResizablePanel>
     </ResizablePanelGroup>
   );
