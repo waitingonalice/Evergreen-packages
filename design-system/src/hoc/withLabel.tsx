5d2aabaea146745ee93c1b44df61a3b6a429b0a6
Wilson <fcsie55@gmail.com>
2024-01-28 21:07:10 +0800

    chore: improve prop name

--- design-system/src/hoc/withLabel.tsx
+++ design-system/src/hoc/withLabel.tsx
@@ -3,22 +3,22 @@ import clsx from "clsx";
 import { Text } from "../components";
 
 export interface LabelProps {
-  title?: string;
   id?: string;
+  label?: string;
+  subLabel?: string;
   required?: boolean;
   className?: string;
-  subTitle?: string;
 }
 
-const Label = ({ title, subTitle, id, required, className }: LabelProps) => (
+const Label = ({ label, subLabel, id, required, className }: LabelProps) => (
   <span className={clsx("flex flex-col mb-1", className)}>
     <label htmlFor={id} className="text-secondary-5 flex gap-x-1">
-      <Text type="body">{title}</Text>
+      <Text type="body">{label}</Text>
       {required ? <span className="text-error-main">*</span> : null}
     </label>
-    {subTitle && (
+    {subLabel && (
       <Text type="caption" className="text-secondary-4">
-        {subTitle}
+        {subLabel}
       </Text>
     )}
   </span>
@@ -26,12 +26,12 @@ const Label = ({ title, subTitle, id, required, className }: LabelProps) => (
 
 export const withLabel = <C,>(Component: React.FC<C>) => {
   const withLabelComponent = forwardRef((props: LabelProps & C, ref) => {
-    const { title, subTitle } = props;
+    const { label, subLabel } = props;
 
-    if (!title) return <Component {...props} ref={ref} />;
+    if (!label) return <Component {...props} ref={ref} />;
 
     return (
-      <div className={clsx("flex flex-col", { "gap-y-1": subTitle })}>
+      <div className={clsx("flex flex-col", { "gap-y-1": subLabel })}>
         <Label {...props} />
         <Component {...props} ref={ref} />
       </div>
