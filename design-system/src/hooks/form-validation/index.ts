bf2e68fa828ec520b553fe312a891623af64241d
Wilson <fcsie55@gmail.com>
2024-01-30 13:55:45 +0800

    chore: integrate useform

---
+++ design-system/src/hooks/form-validation/index.ts
@@ -0,0 +1,120 @@
+/* eslint-disable no-underscore-dangle */
+import { useState } from "react";
+import isNumber from "lodash/isNumber";
+import type { ZodEffects, ZodObject, ZodSchema } from "zod";
+import { findKey } from "../../utils";
+
+interface UseFormType<T> {
+  zod: ZodObject<any> | ZodEffects<ZodObject<any>>;
+  data: T;
+}
+export type Error<T> = Record<keyof T, string>;
+/**
+ * @param options - options object
+ * @param options.zod - zod schema to be used for validation
+ * @param options.data - data to be validated
+ *
+ * @description useForm is a custom hook that uses zod to validate form data. The property names of the data object must match the property names of the zod schema.
+ */
+export const useForm = <T extends Record<string, any>>({
+  zod,
+  data,
+}: UseFormType<T>) => {
+  const emptyErrorState = {} as Error<T>;
+  const [errors, setErrors] = useState<Error<T>>(emptyErrorState);
+
+  const schemaKeys: string[] =
+    "keyof" in zod
+      ? zod.keyof()._def.values
+      : zod._def.schema.keyof()._def.values;
+
+  const subsetSchema = (key: string) =>
+    "pick" in zod
+      ? zod.pick({ [key]: true })
+      : zod._def.schema.pick({ [key]: true });
+
+  // zod middleware to customise default error messages
+  const middleware = () => {
+    schemaKeys.forEach((key) => {
+      const singleSchema = subsetSchema(key);
+      if (!singleSchema) return;
+      const { shape } = singleSchema._getCached();
+      const schemaDefinitions = findKey("checks", shape) as
+        | Array<{
+            kind: string;
+            value: number;
+            message?: string;
+          }>
+        | undefined;
+      if (!schemaDefinitions) return;
+
+      for (let i = 0; i < schemaDefinitions.length; i += 1) {
+        const schemaCheck = schemaDefinitions[i];
+        const hasCustomMessage = "message" in schemaCheck;
+        // custom message goes here
+        if (schemaCheck.kind === "min" && !hasCustomMessage) {
+          schemaCheck.message = "This field is required.";
+        }
+
+        if (schemaCheck.kind === "max" && !hasCustomMessage) {
+          schemaCheck.message = `This field cannot be longer than ${schemaCheck.value} characters.`;
+        }
+      }
+    });
+  };
+
+  middleware();
+
+  return {
+    /**
+     * @param customSchema - optional custom schema to be used for validation instead of the default schema provided.
+     * E.g. To evaluate data in the form of an array of objects
+     */
+    /**
+     * @params id - the id of the element to be validated
+     * @params value - the value of the element to be validated
+     */
+    validate: (id: keyof T | string, value: string) => {
+      if (!zod) return "";
+      const checkValue = isNumber(value) ? Number(value) : value ?? "";
+      const singleSchema = subsetSchema(id as string);
+      const result = singleSchema.safeParse({
+        [id]: checkValue,
+      });
+
+      if (result && !result.success) {
+        const {
+          error: { issues: data },
+        } = result;
+        setErrors((prev) => ({ ...prev, [id]: data[0].message }));
+        return data[0].message;
+      }
+      setErrors((prev) => ({ ...prev, [id]: "" }));
+      return "";
+    },
+
+    onSubmitValidate<P extends ZodSchema>(customSchema?: P) {
+      const result = customSchema
+        ? customSchema.safeParse(data)
+        : zod.safeParse(data);
+      if (!result.success) {
+        const submissionErrors = result.error.issues.reduce(
+          (acc, curr) => ({
+            ...acc,
+            [curr.path[0]]: curr.message,
+          }),
+          {} as typeof errors
+        );
+        setErrors(submissionErrors);
+        return result.success;
+      }
+      return result.success;
+    },
+
+    errors,
+
+    clearErrors() {
+      setErrors(emptyErrorState);
+    },
+  };
+};
