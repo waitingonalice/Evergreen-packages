1e8199009dfba93c6f193b7538ecb6f1e2cd6693
Wilson <fcsie55@gmail.com>
2024-05-18 15:01:35 +0800

    chore: update dialog component to remove size

--- design-system/src/components/dialog/index.tsx
+++ design-system/src/components/dialog/index.tsx
@@ -28,66 +28,47 @@ DialogOverlay.propTypes = DialogPrimitive.Overlay.propTypes;
 interface DialogContentProps
   extends React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> {
   onClose?: () => void;
-  size?: "sm" | "md" | "lg" | "xl" | "fullscreen";
   disableInteractOutside?: boolean;
 }
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   DialogContentProps
->(
-  (
-    {
-      className,
-      children,
-      size = "sm",
-      disableInteractOutside,
-      onClose,
-      ...props
-    },
-    ref
-  ) => {
-    const sizeMapper: Record<typeof size, string> = {
-      sm: "max-w-sm",
-      md: "max-w-md",
-      lg: "max-w-lg",
-      xl: "max-w-xl",
-      fullscreen: "max-w-full",
-    };
-    const handleClose = (event: Event) => {
-      if (disableInteractOutside) {
-        event.preventDefault();
-        return;
-      }
-      onClose?.();
-    };
-    return (
-      <DialogPortal>
-        <DialogOverlay />
-        <DialogPrimitive.Content
-          {...props}
-          ref={ref}
-          className={cn(
-            "fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg bg-white",
-            sizeMapper[size],
-            className
-          )}
-          onInteractOutside={handleClose}
-        >
-          {children}
-          {onClose && (
-            <Button
-              onClick={onClose}
-              className="absolute right-4 top-4 rounded-sm opacity-70  transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-primary-main focus:ring-offset-2 disabled:pointer-events-none"
-            >
-              <X className="h-4 w-4 " />
-              <span className="sr-only">Close</span>
-            </Button>
-          )}
-        </DialogPrimitive.Content>
-      </DialogPortal>
-    );
-  }
-);
+>(({ className, children, disableInteractOutside, onClose, ...props }, ref) => {
+  const handleClose = (event: Event) => {
+    if (disableInteractOutside) {
+      event.preventDefault();
+      return;
+    }
+    onClose?.();
+  };
+  return (
+    <DialogPortal>
+      <DialogOverlay />
+      <DialogPrimitive.Content
+        {...props}
+        ref={ref}
+        className={cn(
+          "fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg bg-white",
+          "h-full w-full sm:h-fit sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl",
+          className
+        )}
+        onInteractOutside={handleClose}
+      >
+        {children}
+        {onClose && (
+          <Button
+            variant="primaryLink"
+            onClick={onClose}
+            className="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-primary-main focus:ring-offset-2 disabled:pointer-events-none"
+          >
+            <X className="h-4 w-4" />
+            <span className="sr-only">Close</span>
+          </Button>
+        )}
+      </DialogPrimitive.Content>
+    </DialogPortal>
+  );
+});
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 interface DialogBodyProps {
