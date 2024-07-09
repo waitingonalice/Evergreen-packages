a77ba69ac0a12ff36bd45c29ae082cb9912d31f2
Wilson <fcsie55@gmail.com>
2024-07-09 15:03:36 +0800

    fix lint error

--- design-system/src/hooks/delay-unmount/index.ts
+++ design-system/src/hooks/delay-unmount/index.ts
@@ -3,8 +3,7 @@ import { useEffect, useState } from "react";
 export const useDelayUnmount = (isMounted: boolean, delayTime: number) => {
   const [render, setRender] = useState(false);
   useEffect(() => {
-    let timeout: NodeJS.Timeout | null = null;
-    timeout = setTimeout(() => {
+    const timeout = setTimeout(() => {
       setRender(isMounted);
     }, delayTime);
     return () => clearTimeout(timeout);
