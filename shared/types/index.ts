4854693f256f0f67565234830f60a2f51e548296
Wilson <fcsie55@gmail.com>
2024-08-15 14:39:14 +0800

    chore: add generic types

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -1,1 +1,8 @@
 export type PrimitiveType = string | number;
+
+export type Maybe<T> = T | null;
+
+export type ExtractClassProperties<C> = {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  [Key in keyof C as C[Key] extends Function ? never : Key]: C[Key];
+};
